bebc8015e8f28b5c1fc95f34770591ce754a6b54
David Biolchini <biolchinid@gmail.com>
2021-01-05 20:56:18 -0300

    adicionando libs

--- pokedex/src/Pages/Router.js
+++ pokedex/src/Pages/Router.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from './Home.js'
+import Pokedex from './Pokedex.js'
 
 export default function Router() {
     return (
         <div>
-            
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path={"/"}>
+                        <Home />
+                    </Route>
+                    
+                    <Route exact path={"/pokedex"}>
+                        <Pokedex />
+                    </Route>
+                    
+                    <Route>
+                        <div>Erro 404 - Página não encontrada</div>
+                    </Route>
+                </Switch>
+            </BrowserRouter>
         </div>
     )
 }
